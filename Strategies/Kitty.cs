86183a661e9116176afe3802af73b5567e1f985a
Phuong Phan <phanphuong2503@gmail.com>
2025-03-07 09:59:24 -0600

    Add log to clarify

--- Strategies/Kitty.cs
+++ Strategies/Kitty.cs
@@ -100,7 +100,7 @@ namespace NinjaTrader.NinjaScript.Strategies
                 3. Volume sau cao hơn volume trước: [{currentWAE.DownTrendVal > previousWAE.DownTrendVal}], 
                 4. Volume sau cao hơn DeadZone: (See 1)
                 5. Nến ĐỎ, Thân nến hiện tại > 5 points và < 60 pts: [{isRedCandle}]
-                6. Thân cây nến trước không quá 60pts (open: {prev_openPrice_5m:N2}, close: {prev_closePrice_5m:N2}): [{previousBody}]
+                6. Thân cây nến trước không quá 60pts (open: {prev_openPrice_5m:N2}, close: {prev_closePrice_5m:N2}, body: {Math.Abs(prev_closePrice_5m - prev_openPrice_5m):N2}): [{previousBody}]
                 7. RSI > 30 (Not oversold): [{rsi_5m > 30}], 
                 8. Râu nến phía DƯỚI không quá 40% toàn cây nến: [{bottomToBody}].
                 9. KHÔNG ĐƯỢC THỎA MÃN điều kiện: Nến trước là XANH và có body > 50% cây nến gần nhất.[{!previousIsGreenAndTooStrong_FORSELL}]
@@ -156,7 +156,7 @@ namespace NinjaTrader.NinjaScript.Strategies
                 3. Volume sau cao hơn volume trước: [{currentWAE.UpTrendVal > previousWAE.UpTrendVal}], 
                 4. Volume sau cao hơn DeadZone: (See 1)
                 5. Nến XANH, Thân nến hiện tại > 5 points và < 60 pts: [{isGreenCandle}]
-                6. Thân cây nến trước không quá 60pts (open: {prev_openPrice_5m:N2}, close: {prev_closePrice_5m:N2}): [{previousBody}]
+                6. Thân cây nến trước không quá 60pts (open: {prev_openPrice_5m:N2}, close: {prev_closePrice_5m:N2}, body: {Math.Abs(prev_closePrice_5m - prev_openPrice_5m):N2})): [{previousBody}]
                 7. RSI < 70 (Not overbought): [{rsi_5m < 70}], 
                 8. Râu nến phía TRÊN không quá 40% toàn cây nến: [{topToBody}].
                 9. KHÔNG ĐƯỢC THỎA MÃN điều kiện: Nến trước là ĐỎ và có body > 50% cây nến gần nhất. [{!previousIsRedAndTooStrong_FORBUY}]
